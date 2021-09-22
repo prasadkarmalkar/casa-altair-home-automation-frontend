21f0bf0222b81aeb9515e0f347da971fede876e4
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2021-09-22 12:57:47 +0530

    All menu and appliance are connected to backend

--- js/index..js
+++ js/index..js
@@ -1,75 +1,16 @@
 
+function fetch_custom(url, options, timeout = 5000) {
+    return Promise.race([
+        fetch(url, options),
+        new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('timeout')), timeout)
+        )
+    ]);
+}
+
 const BASE_URL = 'https://webappdemo.casaaltair.com'
 
 
-function sendRequest(event) {
-    event.preventDefault();
-    var url = "https://webappdemo.casaaltair.com/appliance"
-    var method = "POST"
-    var data = {
-        room: 'living room',
-        appliance: event.target.value,
-        app_status: event.target.checked ? "on" : "off"
-    }
-    // Sending The Requeest To Server
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-        // Request finished.
-        if (this.status === 200) {
-            event.target.checked = !event.target.checked;
-            // Loging The Response
-            console.log(this.responseText)
-            // Checking which Radio Button is Clicked And Changing The Class 
-            switch (event.target.value) {
-                case 'air_conditioner':
-                    var x = document.getElementById('air_conditioner');
-                    if (event.target.checked === true) {
-                        x.classList.add('appliance__active');
-                    } else {
-                        x.classList.remove('appliance__active');
-                    }
-                    break;
-                case 'led_bulb':
-                    var x = document.getElementById('led_bulb');
-                    if (event.target.checked === true) {
-                        x.classList.add('appliance__active');
-                    } else {
-                        x.classList.remove('appliance__active');
-                    }
-                    break;
-                case 'television':
-                    var x = document.getElementById('television');
-                    if (event.target.checked === true) {
-                        x.classList.add('appliance__active');
-                    } else {
-                        x.classList.remove('appliance__active');
-                    }
-                    break;
-                case 'speaker':
-                    var x = document.getElementById('speaker');
-                    if (event.target.checked === true) {
-                        x.classList.add('appliance__active');
-                    } else {
-                        x.classList.remove('appliance__active');
-                    }
-                    break;
-
-            }
-        } else {
-            // If Anything Occur Wrong Then Alert Will Show
-            alert('Communication Error')
-        }
-    };
-    xhttp.ontimeout = function (e) {
-        // If Timeout Then Alert Will Show
-        alert('Communication Error')
-    };
-
-    xhttp.open("POST", url);
-    xhttp.timeout = 5000;
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(JSON.stringify(data));
-}
 
 // To Make Menu Scrollable
 const menu = document.querySelector('.sub__heading ul');
@@ -96,13 +37,122 @@ menu.addEventListener('mousemove', (e) => {
 });
 
 
-// Fetch Initial Data From Server
-const getInitialData = ()=>{
+// fetch_custom Initial Data From Server
+const allappliances = document.querySelector('.all__appliances');
+let jsondata;
+async function getInitialData() {
+
     let url = BASE_URL + '/control'
-    console.log(url)
-    fetch(url).then(respone =>{
-        console.log(respone)
-    })    
+    try {
+        let rawdata = await fetch_custom(url)
+        if (rawdata.status !== 200) {
+            alert("Communication Error");
+            return;
+        }
+        jsondata = await rawdata.json()
+        // Displaying Menu 
+        jsondata["Room Name"].forEach(element => {
+            if (element.room_id == 1) {
+                menu.innerHTML = menu.innerHTML + "<li class='active'>" + element.room_name + "</li>";
+            }
+            else {
+                menu.innerHTML = menu.innerHTML + "<li>" + element.room_name + "</li>";
+            }
+        });
+        // Displaying all appliances
+        let appliancesrawhtml = jsondata["Appliance Data"].map((appliance) => {
+            var startingdiv = `<div class="appliance" id="${appliance.appliance_id}">`;
+            var inputtag = `<input type="checkbox" value="${appliance.appliance_id}" onclick="sendRequest(event)"></input>`
+            if (appliance.appliance_condition == 1) {
+                startingdiv = `<div class="appliance appliance__active" id="${appliance.appliance_id}">`
+                inputtag = `<input type="checkbox" value="${appliance.appliance_id}" onclick="sendRequest(event)" checked></input>`
+            }
+            let applianceraw =
+                `${startingdiv}    
+                    <img src="./img/icons/air_conditioner.svg" alt="Air Conditioner" class="appliance__icon">
+                    <h2 class="appliance__heading">${appliance.appliance_name}</h2>
+                        <div class="radio__button">
+                            <label class="switch">
+                                ${inputtag}
+                            <span class="slider round"></span>
+                            </label>
+                        </div>
+                </div>`;
+            return applianceraw;
+        }).join("");
+        allappliances.innerHTML = appliancesrawhtml;
+
+    } catch (error) {
+        alert("Communication Error");
+        console.error(error);
+    }
 
 }
 getInitialData();
+
+async function sendRequest(event) {
+    event.preventDefault();
+
+    var url = BASE_URL + "/appliance"
+    var method = "POST"
+    var data = {
+        room: 'living room',
+        appliance: event.target.value,
+        app_status: event.target.checked ? "on" : "off"
+    }
+    if (jsondata) {
+        var cappliance = jsondata["Appliance Data"].map((appliance) => {
+            if (appliance.appliance_id == event.target.value) {
+                var condition_changed = 0;
+                if (appliance.appliance_condition == 0) {
+                    condition_changed = 1;
+                }
+                var tempdata = {
+                    room_id: 1,
+                    appliance_id: appliance.appliance_id,
+                    appliance_condition: condition_changed
+                }
+
+                fetch_custom(url, {
+                    // Adding method type
+                    method: "POST",
+                    // Adding body or contents to send
+                    body: JSON.stringify(tempdata),
+                    // Adding headers to the request
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                }).then(response => {
+                    if (response.status !== 200) {
+                        alert("Communication Error");
+                        return;
+                    }
+                    return response.json()
+                })
+                    .then(json => {
+                        if (json.status != '200') {
+                            alert("Communication Error");
+                            return;
+                        }
+                        jsondata["Appliance Data"].forEach(element => {
+                            if (element.appliance_id == event.target.value) {
+                                event.target.checked = !event.target.checked;
+                                var x = document.getElementById(element.appliance_id);
+                                if (event.target.checked === true) {
+                                    x.classList.add('appliance__active');
+                                } else {
+                                    x.classList.remove('appliance__active');
+                                }
+                            }
+                        });
+                        jsondata["Appliance Data"] = json["Appliance Data"];
+
+                    }).catch(error => {
+                        alert("Communication Error");
+                    })
+            }
+        })
+    } else {
+        alert("Something Went Wrong");
+    }
+    }
